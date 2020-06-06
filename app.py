677356e230994bd9b7793613f559f9bd5fa282ee
Simon Rydebrink (PC) <dat14sry@student.lu.se>
2020-06-06 21:44:51 +0200

    [Day 2] second 2 tasks functioning in back-end and have figured out how to handle and manipulate data in front-end

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import functools
 import collections
 import operator
 import datetime
+import calendar
 from dateutil.parser import parse
 import os
 from dotenv import load_dotenv
@@ -13,6 +14,10 @@ load_dotenv()
 
 API_KEY = os.getenv('PROJECT_API_KEY')
 
+STATUS_CUSTOMER = 3
+STATUS_INACTIVE = 2
+STATUS_PROSPECT = 1
+STATUS_IRRELEVANT = 0
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -69,6 +74,10 @@ def filter_deals(data):
         if (entry.get("_embedded") is not None):
             deal["Customer"] = entry.get("_embedded").get(
                 "relation_company").get("name")
+            deal["Customer-id"] = entry.get("_embedded").get(
+                "relation_company").get("_id")
+            deal["company_status"] = entry.get("_embedded").get(
+                "relation_company").get("buyingstatus").get("key")
         deals.append(deal)
     return deals
 
@@ -105,7 +114,6 @@ def get_average_per_year(data):
         if status == "agreement":
             value = deal.get("value")
             collected_data[year].append(value)
-    print(collected_data)
     for year in collected_data:
         count = 0
         total = 0
@@ -113,8 +121,8 @@ def get_average_per_year(data):
             total += value
             count += 1
         collected_data[year] = round((total / count), 2)
-        return_data.append({year: collected_data[year], "Total deals": count})
-    print(collected_data)
+        return_data.append(
+            {"year": year, "avg_value": collected_data[year], "total_deals": count})
     return return_data
 
 
@@ -142,22 +150,21 @@ def get_average_per_month(data, year):
         deal_year = deal.get("Closing date").year
         status = deal.get("status")
         if deal_year == year and status == "agreement":
-            month = deal.get("Closing date").strftime("%B")
+            month = deal.get("Closing date").month
             collected_data[month].append(1)
-    print(collected_data)
     for month in collected_data:
         count = 0
         for value in collected_data[month]:
             count += 1
         collected_data[month] = count
         return_data.append(
-            {month: collected_data[month], "Total deals": count})
-    print(collected_data)
-    return return_data
+            {"month": month, "name": calendar.month_name[month], "total_deals": collected_data[month]})
+    return sorted(return_data, key=operator.itemgetter("month"))
 
 
-@app.route('/average_month')
-def average_month(year=datetime.datetime(2019, 1, 1).year):
+@app.route('/average_month/<pick_year>')
+def average_month(pick_year):
+    year = datetime.datetime(int(pick_year), 1, 1).year
 
     base_url = "https://api-test.lime-crm.com/api-test/api/v1/limeobject/deal/"
     params = "?_limit=50&_embed=company"
@@ -173,6 +180,139 @@ def average_month(year=datetime.datetime(2019, 1, 1).year):
         msg = 'No deals found'
         return render_template('average_month.html', msg=msg)
 
+
+def get_customer_value(data, year):
+    collected_data = collections.defaultdict(list)
+    return_data = []
+    for deal in data:
+        customer = deal.get("Customer")
+        if customer is not None:
+            deal_year = deal.get("Closing date").year
+            status = deal.get("status")
+            if deal_year == year and status == "agreement":
+                value = deal.get("value")
+                collected_data[customer].append(value)
+    for cust in collected_data:
+        total = 0
+        count = 0
+        for value in collected_data[cust]:
+            total += value
+            count += 1
+        collected_data[cust] = total
+        return_data.append(
+            {"Customer": cust, "Value": collected_data[cust], "total_deals": count})
+
+    return sorted(return_data, key=operator.itemgetter("Value"), reverse=True)
+
+
+@app.route('/customer_value/<pick_year>')
+def customer_value(pick_year):
+    year = datetime.datetime(int(pick_year), 1, 1).year
+
+    base_url = "https://api-test.lime-crm.com/api-test/api/v1/limeobject/deal/"
+    params = "?_limit=50&_embed=company"
+    url = base_url + params
+    response_deals = get_api_data(headers=headers, url=url)
+
+    if len(response_deals) > 0:
+        data = filter_deals(response_deals)
+        customers = get_customer_value(
+            data, year)
+        return render_template('customer_value.html', customers=customers, display_year=year)
+    else:
+        msg = 'No deals found'
+        return render_template('average_month.html', msg=msg)
+
+
+def filter_customers(data):
+    customers = []
+    for entry in data:
+        customer = {
+            "id": entry.get("_id"),
+            "name": entry.get("name"),
+        }
+        customers.append(customer)
+    print(customers)
+    return customers
+
+
+def subtract_years(dt, years):
+    try:
+        dt = dt.replace(year=dt.year-years)
+    except ValueError:
+        dt = dt.replace(year=dt.year-years, day=dt.day-1)
+    return dt
+
+
+def set_customer_status(data):
+    status = max(data)
+
+    if status == STATUS_CUSTOMER:
+        return "Customer"
+    elif status == STATUS_INACTIVE:
+        return "Inactive"
+    elif status == STATUS_IRRELEVANT:
+        return "Irrelevant"
+    elif status == STATUS_PROSPECT:
+        return "Prospect"
+    else:
+        return "Unknown status"
+
+
+def get_customer_status(deals):
+    collected_data = collections.defaultdict(list)
+    return_data = []
+    last_year = subtract_years(datetime.datetime.now(datetime.timezone.utc), 1)
+    for data in deals:
+        if data.get("Customer-id") is not None:
+            customer = data.get("Customer")
+            customer_id = data.get("Customer-id")
+            deal_id = data.get("id")
+            deal_status = data.get("status")
+            deal_date = data.get("Closing date")
+            customer_status = data.get("company_status")
+            if deal_status == "agreement":
+                if deal_date > last_year:
+                    deal_value = STATUS_CUSTOMER
+                else:
+                    deal_value = STATUS_INACTIVE
+            elif customer_status == "irrelevant":
+                deal_value = STATUS_IRRELEVANT
+            else:
+                deal_value = STATUS_PROSPECT
+            print(customer, customer_id, deal_id,
+                  deal_status, deal_date, deal_value)
+            collected_data[customer].append(deal_value)
+    for comp in collected_data:
+        print("Customer", comp, "Deals", collected_data[comp], "Status", set_customer_status(
+            collected_data[comp]))
+        return_data.append(
+            {"Customer": comp, "Status": set_customer_status(collected_data[comp])})
+    return return_data
+
+
+@ app.route('/customer_status')
+def customer_status():
+    deal_api = "https://api-test.lime-crm.com/api-test/api/v1/limeobject/deal/"
+    deal_params = "?_limit=50&_embed=company"
+    deal_url = deal_api + deal_params
+
+    # company_api = "https://api-test.lime-crm.com/api-test/api/v1/limeobject/company/"
+    # company_params = "?_limit=50"
+    # company_url = company_api + company_params
+
+    response_deals = get_api_data(headers=headers, url=deal_url)
+    # response_company = get_api_data(headers=headers, url=company_url)
+
+    if len(response_deals) > 0:
+        deals = filter_deals(response_deals)
+        # customer_info = filter_customers(response_company)
+        customers_status = get_customer_status(
+            deals)
+        return render_template('customer_status.html', customers=customers_status)
+    else:
+        msg = 'No deals found'
+        return render_template('customer_status.html', msg=msg)
 
 # You can add more pages to your app, like this:
 #
